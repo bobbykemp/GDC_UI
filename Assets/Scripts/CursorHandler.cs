beb267cb59b7284816b38daab70c39f4f4d27c8d
bobbykemp <robert.kemp@mavs.uta.edu>
2018-02-07 11:26:41 -0600

    First Commit
    Includes UIHandler script for a simple pause screen with continue button and a CursorHandler script for altering the cursor when it hovers over different objects in the world space

---
+++ Assets/Scripts/CursorHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHandler : MonoBehaviour {
+
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+	public GameObject other;
+	//Use raycasts to see what the pointer is hovering over in the GUI
+	Ray ray;
+	RaycastHit hit;
+
+	//use to select which tag will cause a mouse cursor change
+	private string target_name = "Target";
+
+	void Start () {
+		
+	}
+
+	//using a chain of else-ifs or a switch statement, multiple character types
+	//can be handled here, for instance, if hovering over a friendly npc,
+	//the mouse cursor could display a talk bubble, assuming they have a collider attached
+	//otherwise, refactoring will be necessary
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit))
+		{
+			//Debug.Log (hit.collider.name);
+			if(hit.collider.gameObject.tag == target_name)
+			{ Cursor.SetCursor(cursorTexture, hotSpot, cursorMode); }
+		}
+	}
+}
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour {
+
+	public Canvas canvas;
+	private Button button;
+
+	void Start () {
+		//UI = GetComponentInChildren<Canvas> ();
+		button = GetComponentInChildren<Button>();
+		button.onClick.AddListener(ContinueClicked);
+		canvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("escape")) {
+			//Debug.Log ("esc pressed");
+			PauseGame ();
+		}
+	}
+
+	void PauseGame (){
+		if (canvas.enabled) { //if paused unpause
+			canvas.enabled = false;
+			Time.timeScale = 1.0f;
+		} else {
+			canvas.enabled = true;
+			Time.timeScale = 0f;
+		}
+	}
+
+	void ContinueClicked(){
+		//Debug.Log ("button clicked");
+		PauseGame();
+	}
+}
